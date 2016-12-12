53c036a565567563b4113c46d8b316eff9fafe20
Alexandre Tombeur <alexandre.tombeur@gmail.com>
2016-12-12 15:19:56 +0100

    html and css landing page

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,6 @@
 Rails.application.routes.draw do
+  get '/', to: 'pages#home'
+  get 'thankyou', to: 'pages#thankyou'
+
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
